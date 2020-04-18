79087abcbf6b719dc5a463115d3857c2ad9ecfb5
Connor Clark <cjamcl@gmail.com>
2020-04-18 01:02:56 -0700

    cache summary. refactor show to use group by and reduce fns

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import argparse
 import re
 import os
 import glob
+import json
+from collections import namedtuple
 from pprint import pprint
 from types import SimpleNamespace
 
@@ -46,34 +48,73 @@ files.reverse()
 #   'C:/Users/Connor/games/Age of Empires 2 DE/76561198009093901/savegame\MP Replay v101.101.35584.0 @2020.03.06 201515 (2).aoe2record',
 # ]
 
+def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
+def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
+
 def load_game(f):
+  # Parsing game data takes a long time, so a summary of the game is cached.
+  summary_path = f'{os.path.dirname(os.path.abspath(__file__))}/.cache/{os.path.basename(f)}.json'
+  if os.path.exists(summary_path):
+    with open(summary_path) as file:
+      return json2obj(file.read())
+  
+  summary, error = _load_game(f)
+  
+  re_result = re.match('.*Replay v([0-9.]+).*', f)
+  if re_result:
+    version = re_result[1]
+  else:
+    version = None
+  
+  header = summary and summary.get_header()
+  players = summary and summary.get_players()
+  lobby = header and header.lobby
+  de = header and header.de
+  restored = header and header.initial.restore_time > 0
+  
+  if players:
+    for i, ach in enumerate(header.ach):
+      players[i]['achievements'] = ach
+
+  if lobby:
+    del lobby.messages
+  if de:
+    del de.players
+    del de.strings
+    del de.strategic_numbers
+    del de.guid
+    de.lobby_name = str(de.lobby_name.value, 'utf8')
+  
+  game = {
+    'de': de,
+    'error': error,
+    'lobby': lobby,
+    'path': f,
+    'players': players,
+    'restored': restored,
+    'time': os.path.getmtime(f),
+    'version': version,
+  }
+  
+  with open(summary_path, 'w') as file:
+    json_str = json.dumps(game, indent=2)
+    file.write(json_str)
+    return json2obj(json_str)
+
+
+def _load_game(f):
   with open(f, 'rb') as data:
     try:
-      s = Summary(data)
+      summary = Summary(data)
       error = None
-      header = s.get_header()
-      del header['map_info'] # tons of data that isn't needed.
     except Exception as e:
-      s = None
+      summary = None
       error = str(e)
-
-    re_result = re.match('.*Replay v([0-9.]+).*', f)
-    if re_result:
-      version = re_result[1]
-    else:
-      version = None
     
     if error:
       print(error, '| path:', f)
     
-    return {
-      'path': f,
-      'time': os.path.getmtime(f),
-      'version': version,
-      'header': s and s.get_header(),
-      'players': s and s.get_players(),
-      'error': error
-    }
+    return summary, error
 
 
 def count_by(objs, fn):
@@ -83,14 +124,14 @@ def count_by(objs, fn):
 
 
 all_games = [load_game(f) for f in files]
-games = [g for g in all_games if g['error'] is None]
-errored_games = [g for g in all_games if g['error'] is not None]
+games = [g for g in all_games if g.error is None]
+errored_games = [g for g in all_games if g.error is not None]
 
 # Error debugging.
 
 games_by_version = {}
 for game in all_games:
-  key = game['version']
+  key = game.version
   if key not in games_by_version:
     games_by_version[key] = []
   games_by_version[key].append(game)
@@ -99,40 +140,66 @@ for game in all_games:
 print(f'{len(games)} / {len(files)} parsed without errors\n')
 for version, games in dict(sorted(games_by_version.items())).items():
   count = len(games)
-  games_with_errors = [g for g in games if g['error'] is not None]
+  games_with_errors = [g for g in games if g.error is not None]
   print(f'Version {version} ({count - len(games_with_errors)} / {count}) parsed without errors')
   for game in games_with_errors:
-    print('\t' , game['error'].replace('\n', ' '))
+    print('\t' , game.error.replace('\n', ' '))
 print()
 
 # Fun stats.
 
 lighthouse_players = ['Hoten', 'paulie__b', 'Whiskey', 'blarp7070', 'MikeB924', 'jazyan11']
 def is_lighthouse_game(game):
-  return len([1 for p in game['players'] if p['name'] in lighthouse_players]) >= 2
+  if game.restored or game.players is None:
+    return False
+  return len([1 for p in game.players if p.name in lighthouse_players]) >= 2
 lighthouse_games = [g for g in games if is_lighthouse_game(g)]
 
-def count_by(games, fn):
-  counts = {}
-  for g in games:
-    results = fn(g)
-    if not isinstance(results, list):
-      results = [results]
-    for result in results:
-      if result not in counts:
-        counts[result] = 0
-      counts[result] += 1
-  return counts
 
-def show(name, fn):
+def aggregate(games, group_by_fn, reduce_fn):
+  results = {}
+
+  for g in games:
+    # Can have multiple groups (ex: multiple winners)
+    groups = group_by_fn(g)
+    if not isinstance(groups, list):
+      groups = [groups]
+    
+    for group in groups:
+      if group not in results:
+        results[group] = None
+      results[group] = reduce_fn(results[group], group, g)
+
+  return results
+
+
+def reduce_count(acc, group, game):
+  return (acc or 0) + 1
+
+
+def resource_reducer(key):
+  def reducer(acc, group, game):
+    player = next(p for p in game.players if p.name == group)
+    cur = getattr(player.achievements, key)
+    return (acc or 0) + cur
+  return reducer
+
+
+def show(name, group_by_fn, reduce_fn=reduce_count):
   print(f'=== {name}')
-  counts = count_by(lighthouse_games, fn)
-  counts = {k: v for k, v in sorted(counts.items(), key=lambda item: -item[1])}
-  for k, v in counts.items():
+  results = aggregate(lighthouse_games, group_by_fn, reduce_fn)
+  results = {k: v for k, v in sorted(results.items(), key=lambda item: -item[1])}
+  for k, v in results.items():
     print(f'  {k}: {v}')
   
   print()
 
-show('winners',     lambda game: [p['name'] for p in game['players'] if p['winner']])
-show('game types',  lambda game: game['header'].lobby.game_type)
-show('lobby',       lambda game: game['header'].de.lobby_name.value)
+
+show('winners',     lambda game: [p.name for p in game.players if p.winner])
+# Achievements don't work yet.
+# show('food',        lambda game: [p.name for p in game.players], resource_reducer('total_food'))
+# show('wood',        lambda game: [p.name for p in game.players], resource_reducer('total_wood'))
+# show('gold',        lambda game: [p.name for p in game.players], resource_reducer('total_gold'))
+# show('stone',       lambda game: [p.name for p in game.players], resource_reducer('total_stone'))
+show('game types',  lambda game: game.lobby.game_type)
+show('lobby',       lambda game: game.de.lobby_name)
